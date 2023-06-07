a120d53c17e32d0e9ada168da1cd99452f0a468f
Ayush-Kmr07 <135893073+Ayush-Kmr07@users.noreply.github.com>
2023-06-08 03:06:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -218,7 +218,7 @@ const Item = mongoose.model("Item",itemsSchema);
   
 
   
-  app.listen(process.env.PORT || 3000,function(){
+  app.listen(3000,function(){
     console.log("Server started on port 3000");
   })
 
